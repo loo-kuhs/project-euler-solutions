fd91c168db891949d5472701ce9dbefb2062c5f5
tofuBear <graffe.c@yahoo.com>
2022-07-30 21:09:40 -0500

    Euler Project

--- pE001.js
+++ pE001.js
@@ -2,21 +2,26 @@
  * * Solución al problema 1 de projecteuler.net
  */
 
-/**
- * * Recorre todos los números del 0 al 1000, y si el número es 
- * * divisible por 3 o 5, lo suma a la suma.
- * @returns 233168
- */
+const NATURAL_NUMBER = 1000;
 
-const multiples = () => {
+/**
+ * * Toma un número y dos múltiplos, y devuelve la suma de todos
+ * * los números debajo del número que son múltiplos de los dos múltiplos.
+ * @param number - el numero a contar hasta
+ * @param multiple1 - 3
+ * @param multiple2 - 3
+ * @returns La suma de todos los múltiplos de 3 y 5 menores de 1000.
+ */
+const sumMultiples = (number, multiple1, multiple2) => {
   let sum = 0;
-  for (let i = 0; i < 1000; i++) {
-    if (i % 3 === 0 || i % 5 === 0) {
+  if (multiple1 < 0 || multiple2 < 0) throw new Error("Solo numeros positivos");
+  for (let i = 0; i < number; i++) {
+    if (i % multiple1 === 0 || i % multiple2 === 0) {
       sum += i;
     }
   }
   return sum;
 };
 
-let res = multiples();
-console.log(res)
+let result = sumMultiples(NATURAL_NUMBER, 3, 5);
+console.log(result);
