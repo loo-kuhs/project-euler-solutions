0216ac70106bd3055878ac4428b5c6d6448c4798
lucasbear <lucasbear.yo@outlook.com>
2022-10-11 23:35:27 -0500

    updated pE007

--- pE007.js
+++ pE007.js
@@ -2,30 +2,30 @@
  * * Solución al problema 7 de projecteuler.net
  */
 
-/* 
- * * Importar la función `isPrime` del archivo `miLibreria.js`. 
+/**
+ * Importar la función `isPrime` del archivo `Library.js`.
  */
-import { isPrime } from "./Library.js";
+import { isPrime } from "./libs/Library.js";
 
 const PRIME = 10001;
 
 /**
- * * Incremente num hasta que sea primo, luego incremente i hasta 
- * * que sea igual al número pasado.
+ * Incremente num hasta que sea primo, luego incremente i hasta que sea igual al número pasado.
  * @param number - El número del primo que quieres encontrar.
  * @returns El n-ésimo número primo.
  */
-const nthPrime = (number) =>  {
-  let num = 1, i = 0;
+const nthPrime = (number) => {
+  let num = 1,
+    i = 0;
   while (i < number) {
     num++;
     while (!isPrime(num)) {
-      num++
+      num++;
     }
     i++;
   }
   return num;
-}
+};
 
 let result = nthPrime(PRIME);
 console.log(result);
