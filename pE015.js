9d48461c25e112ae594f89977587727ff93eb087
lucasbear <lucasbear.yo@outlook.com>
2022-10-12 18:57:10 -0500

    updated pE015

---
+++ pE015.js
@@ -0,0 +1,26 @@
+/**
+ * * Solución al problema 15 de projecteuler.net
+ */
+
+const GRID = 20;
+const latticePaths = (() => {
+  let gridSize = 20,
+    grid = [gridSize];
+  for (let i = 0; i <= gridSize; i++) {
+    grid[i] = [gridSize];
+  }
+
+  return (x, y) => {
+    let paths = 0;
+    if (x === 0 || y === 0) return 1;
+    if (x > gridSize || y > gridSize) return -1;
+    if (typeof grid[x][y] === "number") return grid[x][y];
+
+    paths = latticePaths(x - 1, y) + latticePaths(x, y - 1);
+    grid[x][y] = paths;
+    return paths;
+  };
+})();
+
+let result = latticePaths(GRID, GRID);
+console.log(result);
