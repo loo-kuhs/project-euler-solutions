55f95d06c957a66bdb5b1fb13ad59f058dcf5707
lucasbear <lucasbear.yo@outlook.com>
2022-10-11 23:30:40 -0500

    updated pE006

--- PE006.js
+++ PE006.js
@@ -5,10 +5,8 @@
 const NATURAL_NUMBERS = 100;
 
 /**
- * * Para cada número del 1 al número dado, agregue el número a la 
- * * suma y agregue el cuadrado del número a la suma de los 
- * * cuadrados. Luego devuelve el cuadrado de la suma menos la suma 
- * * de los cuadrados
+ * Calcula la diferencia entre la suma de los cuadrados de los primeros números 
+ * naturales y el cuadrado de la suma.
  * @param numbers - El número de números a sumar.
  * @returns La diferencia entre la suma de los cuadrados de los cien primeros números naturales y el
  * cuadrado de la suma.
