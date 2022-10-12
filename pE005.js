57e0dc294a4791f909acf55ac1fa65bf026979af
lucasbear <lucasbear.yo@outlook.com>
2022-10-11 23:25:54 -0500

    updated pE005

--- pE005.js
+++ pE005.js
@@ -5,21 +5,21 @@
 /*
  * * Importar la función `primeFactor` desde el archivo `pE003.js`.
  */
-import { primeFactor } from "./Library.js";
+import { primeFactor } from "./libs/Library.js";
 
 const MIN_MULTIPLE = 1,
   MAX_MULTIPLE = 20;
 
 /**
- * * Toma dos matrices de números y devuelve una nueva matriz de
- * * números que contiene todos los números de las dos matrices de
- * * entrada, pero sin duplicados.
+ * Toma dos matrices de números y devuelve una nueva matriz de números que contiene todos los
+ * números de las dos matrices de entrada, pero sin duplicados.
  * @param mP1 - [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
  * 25 , 26, 27, 28, 29, 30, ... ]
  * @param mP2 - [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
  * 25 , 26, 27, 28, 29, 30, ... ]
  * @returns El resultado de la ordenación por fusión.
  */
+
 const minPolynomial = (mP1, mP2) => {
   let result = [],
     x,
@@ -47,12 +47,10 @@ const minPolynomial = (mP1, mP2) => {
 };
 
 /**
- * * Toma el número más pequeño y encuentra sus factores primos,
- * * luego toma el siguiente número más pequeño y encuentra sus
- * * factores primos, luego multiplica los dos juntos, luego toma el
- * * siguiente número más pequeño y encuentra sus factores primos,
- * * luego multiplica los tres juntos , y así sucesivamente hasta
- * * que haya multiplicado todos los números juntos.
+ * Toma el número más pequeño y encuentra sus factores primos, luego toma el siguiente número
+ * más pequeño y encuentra sus factores primos, luego multiplica los dos juntos, luego toma el
+ * siguiente número más pequeño y encuentra sus factores primos, luego multiplica los tres juntos,
+ * y así sucesivamente hasta que haya multiplicado todos los números juntos.
  * @param min - el número mínimo a incluir en el rango
  * @param max - el número máximo a incluir en el rango
  * @returns El número positivo más pequeño que es divisible por todos los números de min a max.
