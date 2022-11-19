910bf578d9479ab0fc2bdbbcb68b65702df63527
lucasbear <lucasbear.yo@outlook.com>
2022-11-19 00:20:43 -0600

    Added chalk module for console results and solution iteration

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+import chalk from "chalk";
 import * as solution from "./src/handlers/solutionHandler.js";
 
+const solNum = chalk.bold.hex("#fee440");
+const success = chalk.underline.hex("#06d6a0");
+const text = chalk.hex("#f5efff");
 const print = console.log;
 
-// TODO: Add chalk npm module for style in console (https://www.npmjs.com/package/chalk)
 
-print(`Resultado del problema 1: ${solution.pE001}`);
-print(`Resultado del problema 2: ${solution.pE002}`);
-print(`Resultado del problema 3: ${solution.pE003}`);
-print(`Resultado del problema 4: ${solution.pE004}`);
-print(`Resultado del problema 5: ${solution.pE005}`);
-print(`Resultado del problema 6: ${solution.pE006}`);
-print(`Resultado del problema 7: ${solution.pE007}`);
+Object.keys(solution).forEach((result, index) =>
+  print(
+    `${text(
+      `Resultado del problema ${solNum(index + 1)}: ${success(
+        solution[result]
+      )}`
+    )}\n`
+  )
+);
