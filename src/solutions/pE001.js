0086f86ed9bf3211ec176c89ed22228218835459
lucasbear <lucasbear.yo@outlook.com>
2023-01-07 18:16:40 -0600

    Solution 1 Refactorized

--- src/solutions/pE001.js
+++ src/solutions/pE001.js
@@ -2,26 +2,59 @@
  * * Solución al problema 1 de projecteuler.net
  */
 
+/**
+ * Una variable constante que se establece en 1000.
+ */
 const NATURAL_NUMBER = 1000;
 
+class MultipleSum {
+  /**
+   * Crea un instancia de MultipleSum.
+   * @param {Array} multiples - Array de números múltiplos.
+   */
+  constructor(multiples) {
+    this.sum = 0;
+    this.multiples = multiples;
+  }
+
+  /**
+   * Calcula la suma de todos los múltiplos de los números almacenados en la propiedad
+   * multiples que sean menores que number.
+   * @param {number} number - Límite superior para el cálculo de los múltiplos. 
+   * @returns {number} La suma de los múltiplos.
+   */
+  calculate(number) {
+    for (let i = 0; i < number; i++) {
+      for (const multiple of this.multiples) {
+        if (i % multiple === 0) {
+          this.sum += i;
+          break;
+        }
+      }
+    }
+    return this.sum;
+  }
+}
+
 /**
- * Toma un número y dos múltiplos, y devuelve la suma de todos
- * los números debajo del número que son múltiplos de los dos múltiplos.
- * @param number - el número a contar hasta
- * @param multiple1 - 3
- * @param multiple2 - 3
- * @returns La suma de todos los múltiplos de 3 y 5 menores de 1000.
+ * Toma un número y dos múltiplos, y devuelve la suma de todos los múltiplos de
+ * esos dos números hasta el número.
+ * @param {number} number - El número hasta el que se suman los múltiplos de múltiplos.
+ * @param {number} multiple1 - El primer múltiplo en sumar
+ * @param {number} multiple2 - El segundo múltiplo a sumar.
+ * @returns {number} La suma de todos los múltiplos de 3 o 5 por debajo del número pasado.
+ * @throws {Error} Si multiple1 o multiple2 no son números enteros positivos.
  */
 const sumMultiples = (number, multiple1, multiple2) => {
-  let sum = 0;
-  if (multiple1 < 0 || multiple2 < 0)
-    throw new Error("Solo números positivos.").message;
-  for (let i = 0; i < number; i++) {
-    if (i % multiple1 === 0 || i % multiple2 === 0) {
-      sum += i;
-    }
+  if (Number.isInteger(multiple1) && multiple1 > 0) {
+    const multiples = [multiple1, multiple2];
+    const multipleSum = new MultipleSum(multiples);
+    return multipleSum.calculate(number);
+  } else {
+    throw new Error(
+      `El argumento 'multiples' debe ser un número entero mayor que 0`
+    );
   }
-  return sum;
 };
 
 const pE001 = sumMultiples(NATURAL_NUMBER, 3, 5);
